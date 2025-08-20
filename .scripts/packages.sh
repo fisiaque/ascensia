68c1a5a8b96a0dcef4f658bd9a1a75ca2e09793c
fisiaque <fisiaque@gmail.com>
2025-08-20 07:25:27 +0100

    + yay installation packages

--- .scripts/packages.sh
+++ .scripts/packages.sh
@@ -1,12 +1,36 @@
 # THIS BASH FILE MAKES SURE ALL NECCESSARY PACKAGES ARE UPDATED
 #!/bin/bash
+
+## pacman
 packages=(
     "kitty" "firefox" "brightnessctl" "playerctl" "dolphin" 
     "wofi" "pipewire" "wireplumber"
 )
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 for package in "${packages[@]}"; do
     sudo pacman -S --noconfirm --needed "$package"
 done
+
+## yay
+if ! command -v yay &> /dev/null; then
+    mkdir -p /tmp/yay
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    rm -rf /tmp/yay
+fi
+
+# check if yay exists: then install/refresh packages
+if command -v yay &> /dev/null; then
+    yay_packages=(
+        "quickshell-git"
+    )
+
+    yay -Syu --noconfirm
+
+    for yay_package in "${yay_packages[@]}"; do
+        yay -S --noconfirm --needed "$yay_package"
+    done
+fi
